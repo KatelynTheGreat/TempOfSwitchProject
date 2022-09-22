808eb9d32b3a2f76be49c45416c97906ab868b83
The_Great_Katelyn <hill_katelyn@student.mahoningctc.com>
2022-09-22 08:39:50 -0400

    Add project files.

---
+++ TempOfSwitchProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SwitchProjectStart
+{
+    internal class Program
+    {
+        public static void randomEvent()
+        {
+            //Create a random number var then use a switch to cause a "random" event 
+
+            // switch ()
+            // {
+
+            // }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random Event");
+            Console.WriteLine("Would you like to trigger a event? ");
+            string trigger = Console.ReadLine();
+
+            //Add Switch statement to check user answer
+            //switch (trigger)
+            //{
+
+            // }
+            Console.ReadKey();
+
+
+        }
+    }
+}
